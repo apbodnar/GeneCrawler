1f0bff803ccb7fa9a0ea4e69068aad5587ec4a82
Adam Bodnar <adam.bodnar@disney.com>
2014-02-11 15:51:45 -0800

    prettier less buggy rendering

--- renderer.js
+++ renderer.js
@@ -10,75 +10,10 @@ function Renderer(canvas){
 	
 	var MVM = mat4.create();
     var PM = mat4.create();
+    var plane_res = 40;
+    var plane = [];
+    var plane_indices = [];
 	
-	var cube = [
-		-1.0, -1.0,  1.0,
-		 1.0, -1.0,  1.0,
-		 1.0,  1.0,  1.0,
-		-1.0,  1.0,  1.0,
-		-1.0, -1.0, -1.0,
-		-1.0,  1.0, -1.0,
-		 1.0,  1.0, -1.0,
-		 1.0, -1.0, -1.0,
-		-1.0,  1.0, -1.0,
-		-1.0,  1.0,  1.0,
-		 1.0,  1.0,  1.0,
-		 1.0,  1.0, -1.0,
-		-1.0, -1.0, -1.0,
-		 1.0, -1.0, -1.0,
-		 1.0, -1.0,  1.0,
-		-1.0, -1.0,  1.0,
-		 1.0, -1.0, -1.0,
-		 1.0,  1.0, -1.0,
-		 1.0,  1.0,  1.0,
-		 1.0, -1.0,  1.0,
-		-1.0, -1.0, -1.0,
-		-1.0, -1.0,  1.0,
-		-1.0,  1.0,  1.0,
-		-1.0,  1.0, -1.0
-	];
-	
-	var normals = [
-		 0.0,  0.0,  1.0,
-		 0.0,  0.0,  1.0,
-		 0.0,  0.0,  1.0,
-		 0.0,  0.0,  1.0,
-		 0.0,  0.0, -1.0,
-		 0.0,  0.0, -1.0,
-		 0.0,  0.0, -1.0,
-		 0.0,  0.0, -1.0,
-		 0.0,  1.0,  0.0,
-		 0.0,  1.0,  0.0,
-		 0.0,  1.0,  0.0,
-		 0.0,  1.0,  0.0,
-		 0.0, -1.0,  0.0,
-		 0.0, -1.0,  0.0,
-		 0.0, -1.0,  0.0,
-		 0.0, -1.0,  0.0,
-		 1.0,  0.0,  0.0,
-		 1.0,  0.0,  0.0,
-		 1.0,  0.0,  0.0,
-		 1.0,  0.0,  0.0,
-		-1.0,  0.0,  0.0,
-		-1.0,  0.0,  0.0,
-		-1.0,  0.0,  0.0,
-		-1.0,  0.0,  0.0
-    ];
-	
-	var indices = [
-		0, 1, 2,      
-		0, 2, 3,   
-		4, 5, 6,      
-		4, 6, 7,   
-		8, 9, 10,     
-		8, 10, 11, 
-		12, 13, 14,   
-		12, 14, 15,
-		16, 17, 18,   
-		16, 18, 19,
-		20, 21, 22,   
-		20, 22, 23 
-	];
 
     function initGL(canvas) {
 		gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
@@ -139,45 +74,65 @@ function Renderer(canvas){
         gl.useProgram(program);
         program.vertexAttribute = gl.getAttribLocation(program, "inVertex");
         gl.enableVertexAttribArray(program.vertexAttribute);
-		program.normalAttribute = gl.getAttribLocation(program, "inNormal");
-        gl.enableVertexAttribArray(program.normalAttribute);
         program.pmUniform = gl.getUniformLocation(program, "PM");
         program.mvmUniform = gl.getUniformLocation(program, "MVM");
 		program.widthUniform = gl.getUniformLocation(program, "width");
 		program.heightUniform = gl.getUniformLocation(program, "height");
     }
+
+    function generatePlane(){
+    	var two_pi = Math.PI*2;
+    	for(var j=0; j< plane_res; j++){
+    		for(var i=0; i<plane_res; i++){
+    			x = two_pi*(i/plane_res)*((plane_res+1)/plane_res);
+    			y = two_pi*(j/plane_res)*((plane_res+1)/plane_res);
+    			plane = plane.concat([x,y,0]);
+    		}
+    	}
+    }
+
+    function generateIndices(){
+    	for(var j=0; j< plane_res-1; j++){
+    		for(var i=0; i<plane_res-1; i++){
+    			plane_indices = plane_indices.concat([i+(j*plane_res),i+((j+1)*plane_res),i+((j+1)*plane_res)+1]);
+    			plane_indices = plane_indices.concat([i+((j+1)*plane_res)+1,i+(j*plane_res)+1,i+(j*plane_res)]);
+    		}
+    	}
+    }
 	
 	function initBuffers() {
+
+		generatePlane();
+		generateIndices();
+
         cubeBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, cubeBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cube), gl.STATIC_DRAW);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(plane), gl.STATIC_DRAW);
         cubeBuffer.itemSize = 3;
-        cubeBuffer.numItems = 24;
-		
-		normalBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
-        normalBuffer.itemSize = 3;
-        normalBuffer.numItems = 24;
+        cubeBuffer.numItems = plane.length/cubeBuffer.itemSize;
 
         indexBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(plane_indices), gl.STATIC_DRAW);
         indexBuffer.itemSize = 1;
-        indexBuffer.numItems = 36;
+        indexBuffer.numItems = plane_indices.length;
+    }
+
+    function drawCube(PM ,MVM, width, length){
+    	gl.uniformMatrix4fv(program.pmUniform, false, PM);
+		gl.uniformMatrix4fv(program.mvmUniform, false, MVM);
+
+		gl.uniform1f(program.widthUniform, width);
+		gl.uniform1f(program.heightUniform, length);
+
+		gl.drawElements(gl.TRIANGLES, indexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
     }
 	
 	function drawCore(crawler){
 		mat4.identity(MVM);
 		mat4.translate(MVM, MVM, crawler.core.origin);
-		
-		gl.uniformMatrix4fv(program.pmUniform, false, PM);
-        gl.uniformMatrix4fv(program.mvmUniform, false, MVM);
-		
-		gl.uniform1f(program.widthUniform, crawler.core.width);
-		gl.uniform1f(program.heightUniform, crawler.core.width);
 
-        gl.drawElements(gl.TRIANGLES, indexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+        drawCube(PM, MVM, crawler.core.width, crawler.core.width);
 	}
 	
 	function getAngle(a){
@@ -196,23 +151,16 @@ function Renderer(canvas){
 		mat4.identity(MVM);
 		var t2 = mat4.translate(mat4.create(), mat4.create(), origin);
 		var t1 = mat4.translate(mat4.create(), mat4.create(), [0,crawler.chromosome.segments[id].length/2,0]);
-		
+
 		mat4.multiply(MVM,t2,mat4.multiply(mat4.create(),r1,t1));
-		
-		gl.uniformMatrix4fv(program.pmUniform, false, PM);
-		gl.uniformMatrix4fv(program.mvmUniform, false, MVM);
-
-		gl.uniform1f(program.widthUniform, crawler.core.width/4);
-		gl.uniform1f(program.heightUniform, crawler.chromosome.segments[id].length/2);
-
-		gl.drawElements(gl.TRIANGLES, indexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+		drawCube(PM, MVM, crawler.core.width/4, crawler.chromosome.segments[id].length/2);
 	}
 
 	function drawLegs(crawler){
 		for(var i=0; i<crawler.segment_ids.length; i++){
 			for(var j=0; j<crawler.segment_ids[i].length; j++){
 				var id1 = crawler.segment_ids[i][j];
-				id1 && drawSegment(crawler, id1);
+				(id1 !== undefined) && drawSegment(crawler, id1);
 			}
 		}
 	};
@@ -226,8 +174,6 @@ function Renderer(canvas){
         gl.enable(gl.DEPTH_TEST);
 		gl.bindBuffer(gl.ARRAY_BUFFER, cubeBuffer);
         gl.vertexAttribPointer(program.vertexAttribute, cubeBuffer.itemSize, gl.FLOAT, false, 0, 0);
-		gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
-        gl.vertexAttribPointer(program.normalAttribute, normalBuffer.itemSize, gl.FLOAT, false, 0, 0);
 		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
 		mat4.perspective(PM, 45*(Math.PI/180), gl.viewportWidth / gl.viewportHeight, 0.1, 100.0);
 	}
